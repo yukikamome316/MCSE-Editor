96f42a694232a429041a02a0f10e97a471e704b0
Yuki <32415464+Yukikamome316@users.noreply.github.com>
2020-05-10 15:29:59 +0900

    Homeウィンドウの追加

--- Home.xaml.cs
+++ Home.xaml.cs
@@ -10,30 +10,19 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
-
 
 namespace MCSE_Editor_for_Wii_U
 {
     /// <summary>
-    /// MainWindow.xaml の相互作用ロジック
+    /// Home.xaml の相互作用ロジック
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class Home : Window
     {
-        public MainWindow()
+        public Home()
         {
             InitializeComponent();
             MouseLeftButtonDown += (sender, e) => this.DragMove();
-            Window home = new Home();
-            home.WindowStartupLocation = WindowStartupLocation.CenterScreen;
-            home.ShowDialog();
-
-        }
-
-        private void Rectangle_DragEnter(object sender, DragEventArgs e)
-        {
-
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
@@ -45,6 +34,5 @@ namespace MCSE_Editor_for_Wii_U
         {
             WindowState = WindowState.Minimized;
         }
-
     }
 }
