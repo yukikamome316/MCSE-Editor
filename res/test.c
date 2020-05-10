5862c4cd4c50edfeeb077b18fabf0d2e8162e65f
syoch <hacksyoch@gmail.com>
2020-05-10 12:41:54 +0900

    extractのテストを追加

---
+++ res/test.c
@@ -0,0 +1,14 @@
+#include <tiny_stdlib.h>
+#include "msscmp.h"
+
+int main(int argc, char const *argv[])
+{
+    if(argv <= 1){
+        printf("usage: test <path to msscmp :path>");
+        exit(1);
+    }
+    printf("Test    : Try extract\n");
+    extractMsscmp(argv[1]);
+    printf("Done");
+    return 0;
+}
