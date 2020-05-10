8b60e8a6f72597bb4fe784484ab029b18d4911bd
syoch <hacksyoch@gmail.com>
2020-05-10 13:02:22 +0900

    実行可能に

--- res/msscmp.h
+++ res/msscmp.h
@@ -1,16 +1,19 @@
 #ifndef MSSCMP_H
 #define MSSCMP_H
+
+
 #ifdef __cplusplus
 extern "C"{
 #endif
 
 
 
-__declspec( dllexport ) void extractMsscmp(char* path);
+void __attribute__((dllexport)) extractMsscmp (const char* path);
 
 
 
 #ifdef __cplusplus
 }
 #endif
+
 #endif
