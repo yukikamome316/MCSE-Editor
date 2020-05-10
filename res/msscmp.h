f50be031206c4142cdd92147bbd27bc16c9f9622
syoch <hacksyoch@gmail.com>
2020-05-10 16:03:02 +0900

    構造体の分割

--- res/msscmp.h
+++ res/msscmp.h
@@ -1,10 +1,29 @@
 #ifndef MSSCMP_H
 #define MSSCMP_H
 
+#include <tiny_stdlib.h>
 
 #ifdef __cplusplus
 extern "C"{
 #endif
+
+
+
+typedef struct _Entry{
+    uint32_t offset;
+    uint32_t size;
+    uint32_t sampleRate;
+    char fullpath[200];
+    char name[100];
+    char path[100];
+} Entry;
+
+typedef struct _File{
+    FILE* fp;
+    uint32_t entryCount;
+    uint32_t filetableOffset;
+    Entry *entries;
+} File;
 
 
 
