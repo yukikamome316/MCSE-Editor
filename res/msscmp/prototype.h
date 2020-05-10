3b9f5c16ad1174ffda495bf3a1ca7185c3d16de8
syoch <hacksyoch@gmail.com>
2020-05-10 19:57:19 +0900

    GCCでコンパイルに失敗するのを修正

--- res/msscmp/prototype.h
+++ res/msscmp/prototype.h
@@ -6,10 +6,10 @@
 #include <errno.h>
 #include <stddef.h>
 
-errno_t fopen_s(FILE** pFile,const char *filename,const char *mode);
-errno_t strcat_s(char *strDestination,size_t numberOfElements,const char *strSource);
-errno_t strcpy_s(char *restrict dest, size_t destsz, const char *restrict src);
-errno_t strerror_s( char *buf, size_t bufsz, errno_t errnum );
+int fopen_s(FILE** pFile,const char *filename,const char *mode);
+int strcat_s(char *strDestination,size_t numberOfElements,const char *strSource);
+int strcpy_s(char *restrict dest, size_t destsz, const char *restrict src);
+int strerror_s( char *buf, size_t bufsz, int errnum );
 
 #endif /* !defined(_MSC_VER) */
 #endif /* !defined(MSSCMP_PROTYPE_H) */
