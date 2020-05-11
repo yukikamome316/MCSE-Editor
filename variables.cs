defc6811bb367346cfb3ac558c3384c81b78525c
Yuki <32415464+Yukikamome316@users.noreply.github.com>
2020-05-11 09:40:38 +0900

    クラスのメンバ変数を追加

---
+++ variables.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCSE_Editor_for_Wii_U
+{
+    class variables
+    {
+        public static string openFilePath;
+    }
+}
