2d681fd873dc0131fa946ed379f6c71ede27abc3
syoch <hacksyoch@gmail.com>
2020-05-12 16:14:01 +0900

    replace関数の実装、未完成

--- res/test.c
+++ res/test.c
@@ -11,29 +11,35 @@ int main(int argc, char const *argv[])
     //printf("TestLoad: Start\n");
     //extractMsscmp(argv[1]);
     //printf("TestLoad: Passed\n\n");
-    printf("TestSave: Start\n");
+    printf("Test    : Start\n");
     ret=remove("out.msscmp");
-    printf("TestSave: Ret     : dele = %d\n",ret);
+    printf("Test    : Ret     : dele = %d\n",ret);
     if(ret == 1){
-        printf("TestSave: Error   : dele failed");
+        printf("Test    : Error   : dele failed");
         return 1;
     }
     
     ret=loadMsscmp(argv[1]);
-    printf("TestSave: Ret     : load = %d\n",ret);
+    printf("Test    : Ret     : load = %d\n",ret);
     if(ret == 1){
-        printf("TestSave: Error   : load failed");
+        printf("Test    : Error   : load failed");
         return 1;
     }
     
     ret=saveMsscmp("./out.msscmp");
-    printf("TestSave: Ret     : save = %d\n",ret);
+    printf("Test     : Ret     : save = %d\n",ret);
     if(ret == 1){
-        printf("TestSave: Error   : save failed");
+        printf("Test    : Error   : save failed");
         return 1;
     }
     
+    ret=replaceEntryMsscmp("Minecraft/ambient/cave/cave1_fixed/_16278_192512.binka","replace.data");
+    printf("Test    : Ret     : edit = %d\n",ret);
+    if(ret == 1){
+        printf("Test    : Error   : edit failed");
+        return 1;
+    }
 
-    printf("TestSave: Passed\n\n");
+    printf("Test    : Passed\n\n");
     return 0;
 }
