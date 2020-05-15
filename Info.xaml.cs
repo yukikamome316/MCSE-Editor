6469f39c26ca53ccfb7b9b53a35a2d936e5f9f21
Yuki <32415464+Yukikamome316@users.noreply.github.com>
2020-05-16 01:36:13 +0900

    Info画面の追加

---
+++ Info.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MCSE_Editor_for_Wii_U
+{
+    /// <summary>
+    /// Info.xaml の相互作用ロジック
+    /// </summary>
+    public partial class Info : Window
+    {
+        public Info()
+        {
+            InitializeComponent();
+            MouseLeftButtonDown += (sender, e) => this.DragMove();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void TextBlock_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            Process.Start(yt.Text);
+        }
+
+        private void TextBlock_MouseLeftButtonUp_1(object sender, MouseButtonEventArgs e)
+        {
+            Process.Start(gt.Text);
+        }
+
+        private void TextBlock_MouseLeftButtonUp_2(object sender, MouseButtonEventArgs e)
+        {
+            Process.Start(rp.Text);
+        }
+    }
+}
