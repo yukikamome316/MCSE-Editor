6828c510beea3490a245607b928add358ce7b586
syoch <hacksyoch@gmail.com>
2020-05-16 22:03:02 +0900

    replaceの修正(1)

--- msscmp/test.c
+++ msscmp/test.c
@@ -25,91 +25,67 @@ void checkRet(char str[8],int ret)
     }
     puts("\n");
 }
-
-void extractTest()
+void basicTest()
 {
     checkRet(
         "Extract",
         // extract and load by %1
         extractMsscmp(mssPath));
-}
-
-void saveTest()
-{
-    remove("out.msscmp");
-    checkRet(
-        "save    ",
-        // save to %1
-        saveMsscmp(L"./out.msscmp"));
-}
-void replaceTest()
-{
     checkRet(
         "replace ",
         //msscmp %1  ->   %2
         replaceEntryMsscmp(
             L"Minecraft/UI/scroll/_914_9764864.binka",
             L"di.binka"));
+    remove("out.msscmp");
+    checkRet(
+        "save    ",
+        // save to %1
+        saveMsscmp(L"./out.msscmp"));
 }
-
-void binka2wavTest()
+void binkaTest()
 {
+    checkRet(
+        "wav2bink",
+        //convert %1(wav) => %2(binka)
+        wav2binka(
+            L"datas/di.wav",
+            L"datas/do.binka"
+        ));
     checkRet(
         "bink2wav",
         //convert %1(binka) => %2(wav)
         binka2wav(
-            L"di.binka",
-            L"do.wav"
+            L"datas/di.binka",
+            L"datas/do.wav"
         ));
-}
-void wav2binkaTest()
-{
-    checkRet(
-        "wav2bink",
-        //convert %1  ->   %2
-        wav2binka(
-            L"di.wav",
-            L"do.binka"
-        ));
-}
-void basicTest()
-{
-    extractTest();
-    replaceTest();
-    saveTest();
-}
-void binkaTest()
-{
-    wav2binkaTest();
-    binka2wavTest();
 }
 
 int main(int argc, char const *argv[])
 {
-    int ret = 0;
-    if (argc <= 1)
-    {
-        printf("usage: test <path to msscmp :path>");
-        exit(1);
-    }
-    wchar_t msscmpPath[strlen(argv[1]) * 2];
-    size_t converted;
-    ret = mbstowcs_s(&converted, msscmpPath, strlen(argv[1]) * 2, argv[1], strlen(argv[1]) * 2);
-    if (ret != 0)
-    {
-        char error[256];
-        strerror_s(error, 256, errno);
-        printf("Failed to convert MultiByte to WIdeChar: %s\n", error);
-        abort();
-    }
-    mssPath = msscmpPath;
+    //int ret = 0;
+    //if (argc <= 1)
+    //{
+    //    printf("usage: test <path to msscmp :path>");
+    //    exit(1);
+    //}
+    //wchar_t msscmpPath[strlen(argv[1]) * 2];
+    //size_t converted;
+    //ret = mbstowcs_s(&converted, msscmpPath, strlen(argv[1]) * 2, argv[1], strlen(argv[1]) * 2);
+    //if (ret != 0)
+    //{
+    //    char error[256];
+    //    strerror_s(error, 256, errno);
+    //    printf("Failed to convert MultiByte to WIdeChar: %s\n", error);
+    //    abort();
+    //}
+    //mssPath = msscmpPath;
+
     //Test
-    
-    loadMsscmp(mssPath);
-    replaceEntryMsscmp(L"Minecraft/UI/press/_660_9760768.binka",L"di.binka");
+    binkaTest();
+    loadMsscmp(L"../../data/minecraft.msscmp");
+    replaceEntryMsscmp(L"Minecraft/random/drink/_6248_9228288.binka",L"datas/rand_drink.binka");
     saveMsscmp(L"out.msscmp");
-    loadMsscmp(L"out.msscmp");
-    
     //freopen("CON","w",stdout);
     return 0;
 }
