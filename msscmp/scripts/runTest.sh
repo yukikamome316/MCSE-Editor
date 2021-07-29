54ac3902fc12c9dd1dd1c00e9de41f0a69c864a9
syoch <hacksyoch@gmail.com>
2021-07-30 06:32:36 +0900

    Linux向けのクロスビルド用スクリプト公開

--- msscmp/scripts/runTest.sh
+++ msscmp/scripts/runTest.sh
@@ -7,14 +7,17 @@ i686-w64-mingw32-objcopy -I binary -Bi386 -Ope-i386 ../res/binkaencode.exe obj/b
 i686-w64-mingw32-objcopy -I binary -Bi386 -Ope-i386 ../res/binwin.asi obj/binwin.o
 i686-w64-mingw32-objcopy -I binary -Bi386 -Ope-i386 ../res/mss32.dll obj/mss32.o
 
+echo "compiling msscmp library"
 i686-w64-mingw32-g++ \
-  -static \
+  -shared \
   -static-libgcc -static-libstdc++ \
   -I/usr/i686-w64-mingw32/include/ \
-  -o msscmp.exe \
+  -o msscmp.dll \
   -std=c++17 \
-  ../*.cpp ../test.c \
-  obj/*.o
+  obj/*.o ../*.cpp ../test.c
+
+echo "linking test binary"
+i686-w64-mingw32-g++ -static -std=c++17 -o msscmp.exe msscmp.dll ../test.c
 
 echo "running"
 wine ./msscmp.exe
