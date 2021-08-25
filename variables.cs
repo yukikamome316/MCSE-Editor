94ada46cfc2748da4131659c6c01a28645593d45
Yuki <yukikamome316@gmail.com>
2021-08-26 01:05:56 +0900

    変数名の変更

--- variables.cs
+++ variables.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace MCSE_Editor_for_Wii_U
+﻿namespace MCSE_Editor_for_Wii_U
 {
-    class Variables
+    internal class Variables
     {
         public static string openFilePath = "";
         public static string selectedFilePath = "";
